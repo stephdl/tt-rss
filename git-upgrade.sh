0982196cd4160748adf2d60c4497a0b5c2b1a8b7
Stephane de Labrusse <stephdl@de-labrusse.fr>
2016-03-08 23:33:09 +0100

    * Tue Mar 08 2016 stephane de Labrusse <stephdl@de-labrusse.fr> 20160308-git9232283-1 - new git version of the day 20160308

--- git-upgrade.sh
+++ git-upgrade.sh
@@ -1,16 +1,24 @@
 rm -rf *.tar.gz
 DATE=$(date +"%Y%m%d")
-git clone https://github.com/gothfox/Tiny-Tiny-RSS.git Tiny-Tiny-RSS-$DATE
+git clone https://tt-rss.org/git/tt-rss.git Tiny-Tiny-RSS-$DATE
+cd Tiny-Tiny-RSS-$DATE
+git log --format=%H | git log --pretty --stdin --no-walk > CHANGELOG.git
+COMMIT=$(git log -n 1 --pretty=format:"%h")
+cd ../
+
 rm Tiny-Tiny-RSS-$DATE/.git -rf
-tar cvzf tt-rss-$DATE.tar.gz Tiny-Tiny-RSS-$DATE
-rm -rf Tiny-Tiny-RSS-$DATE
+mv Tiny-Tiny-RSS-$DATE Tiny-Tiny-RSS-$DATE-git$COMMIT
+tar cvzf tt-rss-$DATE-git$COMMIT.tar.gz Tiny-Tiny-RSS-$DATE-git$COMMIT
+
+rm -rf Tiny-Tiny-RSS-$DATE-git$COMMIT
 #sed work
 #version=$(grep -sri 'define version' *.spec | sed 's/%define version //gI')
 release=$(grep -sri 'define release' *.spec | sed 's/%define release //gI')
 #write to the spec file
-sed -i "/%define version/c %define version $DATE" *.spec
-sed -i "/changelog/a * $(LC_ALL=C date +"%a %b %d %Y") stephane de Labrusse <stephdl@de-labrusse.fr> $DATE-$release.sme\n- new git version of the day $DATE\n" *.spec
+sed -i "/%define version/c %define version $DATE-git$COMMIT" *.spec
+sed -i "/changelog/a * $(LC_ALL=C date +"%a %b %d %Y") stephane de Labrusse <stephdl@de-labrusse.fr> $DATE-git$COMMIT-$release\n- new git version of the day $DATE\n" *.spec
 echo ""
 echo " Verify if all is well, then commit the changes"
-echo " git add . ; git commit -m '* $(LC_ALL=C date +"%a %b %d %Y") stephane de Labrusse <stephdl@de-labrusse.fr> $DATE-$release - new git version of the day $DATE'"
+echo " git add . -A ; git commit -m '* $(LC_ALL=C date +"%a %b %d %Y") stephane de Labrusse <stephdl@de-labrusse.fr> $DATE-git$COMMIT-$release - new git version of the day $DATE'"
 echo " git push --tags origin branchname"
+
